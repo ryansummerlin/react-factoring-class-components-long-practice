86d8d6f91f0d31f2abee61ef529baa8b658c3764
ryansummerlin <ryansummerlin777@gmail.com>
2023-06-29 23:07:56 -0700

    finished the project, all class components converted to function components

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -1,65 +1,120 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Headers = ({ titles, currentTab, selectTab }) => {
-  const handleClick = (e) => {
-    const idx = parseInt(e.target.id, 10);
-    selectTab(idx);
-  }
+// const Headers = ({ titles, currentTab, selectTab }) => {
+//   const handleClick = (e) => {
+//     const idx = parseInt(e.target.id, 10);
+//     selectTab(idx);
+//   }
 
-  const tabs = titles.map((title, idx) => {
-    const headerClass = (idx === currentTab) ? 'active' : '';
+//   const tabs = titles.map((title, idx) => {
+//     const headerClass = (idx === currentTab) ? 'active' : '';
+
+//     return (
+//       <li
+//         key={idx}
+//         id={idx}
+//         onClick={handleClick}
+//         className={headerClass}
+//       >
+//         {title}
+//       </li>
+//     );
+//   });
+
+//   return (
+//     <ul className='tab-header'>
+//       {tabs}
+//     </ul>
+//   );
+// }
+
+// class Folder extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentTab: 0
+//     };
+//   }
+
+//   selectTab = (num) => {
+//     this.setState({ currentTab: num });
+//   }
+
+//   render() {
+//     const folder = this.props.folders[this.state.currentTab];
+//     const titles = this.props.folders.map((folder) => folder.title);
+
+//     return (
+//       <section className="tabs-section">
+//         <h1>Tabs</h1>
+//         <div className='tabs'>
+//           <Headers
+//             titles={titles}
+//             currentTab={this.state.currentTab}
+//             selectTab={this.selectTab}
+//           />
+//           <div className='tab-content'>
+//             {folder.content}
+//           </div>
+//         </div>
+//       </section>
+//     );
+//   }
+// }
+
+function Folder({ folders }) {
+  const [currentTab, setCurrentTab] = useState(0);
+  const folder = folders[currentTab];
+  const titles = folders.map((folder) => folder.title);
+
+  const Headers = ({ titles, currentTab, selectTab }) => {
+    const handleClick = (e) => {
+      const idx = parseInt(e.target.id, 10);
+      selectTab(idx);
+    }
+
+    const tabs = titles.map((title, idx) => {
+      const headerClass = (idx === currentTab) ? 'active' : '';
+
+      return (
+        <li
+          key={idx}
+          id={idx}
+          onClick={handleClick}
+          className={headerClass}
+        >
+          {title}
+        </li>
+      );
+    });
 
     return (
-      <li
-        key={idx}
-        id={idx}
-        onClick={handleClick}
-        className={headerClass}
-      >
-        {title}
-      </li>
+      <ul className='tab-header'>
+        {tabs}
+      </ul>
     );
-  });
-  
+  }
+
+  const selectTab = function(num) {
+    setCurrentTab(num);
+  }
+
   return (
-    <ul className='tab-header'>
-      {tabs}
-    </ul>
-  );
-}
-
-class Folder extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentTab: 0
-    };
-  }
-  
-  selectTab = (num) => {
-    this.setState({ currentTab: num });
-  }
-  
-  render() {
-    const folder = this.props.folders[this.state.currentTab];
-    const titles = this.props.folders.map((folder) => folder.title);
-    
-    return (
-      <section className="tabs-section">
-        <h1>Tabs</h1>
-        <div className='tabs'>
-          <Headers
-            titles={titles}
-            currentTab={this.state.currentTab}
-            selectTab={this.selectTab}
-          />
-          <div className='tab-content'>
-            {folder.content}
-          </div>
+    <section className="tabs-section">
+      <h1>Tabs</h1>
+      <div className='tabs'>
+        <Headers
+          titles={titles}
+          currentTab={currentTab}
+          selectTab={selectTab}
+        />
+        <div className='tab-content'>
+          {folder.content}
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
 export default Folder;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,56 +1,55 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { toQueryString } from '../utils';
 
-class Weather extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        weather: null
-      };
-    }
+function Weather() {
+    const [weather, setWeather] = useState(null);
 
-    componentDidMount() {
+    useEffect(() => {
+      const pollWeather = async (location) => {
+        let url = 'http://api.openweathermap.org/data/2.5/weather?';
+
+        /* Remember that it's unsafe to expose your API key. (Note that pushing
+        files that include your key to Github will expose your key!) In
+        production, you would definitely save your key in an environment variable,
+        so do that here. Since this project runs in your local environment
+        (localhost), save your key as an environment variable in a .env file in
+        the root directory of your app. You can then access the key here as
+        "process.env.<variable_name>". Make sure to .gitignore your .env file!
+        Also remember to restart your server (i.e., re-run "npm start") whenever
+        you change your .env file. */
+
+        // unfortunately I've been having a terrible time accessing process.env variables. I know it's bad practice
+        // but I'm putting the api key in here anyways bc I don't have time to fix the .env issues
+        const apiKey = 'b65b43cc09af164f099fe5a807d56972';
+        // const apiKey = process.env.REACT_APP_WEATHER_API;
+
+        const params = {
+          lat: location.coords.latitude,
+          lon: location.coords.longitude,
+          appid: apiKey
+        };
+
+        url += toQueryString(params);
+
+        const res = await fetch(url);
+        if (res.ok) {
+          const newWeather = await res.json();
+          setWeather(newWeather);
+        }
+        else {
+          alert ("Check Weather API key!")
+        }
+      }
+
       navigator.geolocation.getCurrentPosition(
-        this.pollWeather,
+        pollWeather,
         (err) => console.log(err),
         { timeout: 10000 }
       );
-    }
+    }, []);
 
-    pollWeather = async (location) => {
-      let url = 'http://api.openweathermap.org/data/2.5/weather?';
 
-      /* Remember that it's unsafe to expose your API key. (Note that pushing
-      files that include your key to Github will expose your key!) In
-      production, you would definitely save your key in an environment variable,
-      so do that here. Since this project runs in your local environment
-      (localhost), save your key as an environment variable in a .env file in
-      the root directory of your app. You can then access the key here as
-      "process.env.<variable_name>". Make sure to .gitignore your .env file!
-      Also remember to restart your server (i.e., re-run "npm start") whenever
-      you change your .env file. */
-      const apiKey = '???';
-
-      const params = {
-        lat: location.coords.latitude,
-        lon: location.coords.longitude,
-        appid: apiKey
-      };
-
-      url += toQueryString(params);
-
-      const res = await fetch(url);
-      if (res.ok) {
-        const weather = await res.json();
-        this.setState({ weather });
-      }
-      else {
-        alert ("Check Weather API key!")
-      }
-    }
-
-  render() {
-    const weather = this.state.weather;
     let content = <div className='loading'>loading weather...</div>;
 
     if (weather) {
@@ -78,7 +77,7 @@ class Weather extends React.Component {
         </div>
       </section>
     );
-  }
+
 }
 
 export default Weather;
